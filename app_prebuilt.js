a0e8662eb99337f1742830ff005a4d206e5d9338
CheZulkhairi <chezulkhairi.abdullah@gmail.com>
2019-03-22 15:58:52 +0000

    Add files via upload

---
+++ app_prebuilt.js
@@ -0,0 +1,377 @@
+var defaultFill = new ol.style.Fill({
+   color: 'rgba(255,255,255,0.4)'
+ });
+ var defaultStroke = new ol.style.Stroke({
+   color: '#3399CC',
+   width: 1.25
+ });
+ var defaultSelectionFill = new ol.style.Fill({
+   color: 'rgba(255,255,0,0.4)'
+ });
+ var defaultSelectionStroke = new ol.style.Stroke({
+   color: '#FFFF00',
+   width: 1.25
+ });
+
+
+
+                    var textStyleCache_st1_21932_waterextent_sofalaprovince={}
+                    var clusterStyleCache_st1_21932_waterextent_sofalaprovince={}
+                    var style_st1_21932_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(66,167,206,1.0)"})
+                        })
+                        ];
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+                    var selectionStyle_st1_21932_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ]
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+
+                    var textStyleCache_st1_219319_waterextent_sofalaprovince={}
+                    var clusterStyleCache_st1_219319_waterextent_sofalaprovince={}
+                    var style_st1_219319_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(45,101,134,1.0)"})
+                        })
+                        ];
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+                    var selectionStyle_st1_219319_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ]
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+
+                    var textStyleCache_st1_219314_waterextent_sofalaprovince={}
+                    var clusterStyleCache_st1_219314_waterextent_sofalaprovince={}
+                    var style_st1_219314_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(234,15,8,1.0)"})
+                        })
+                        ];
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+                    var selectionStyle_st1_219314_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ]
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+
+                    var textStyleCache_st1_219313_waterextent_sofalaprovince={}
+                    var clusterStyleCache_st1_219313_waterextent_sofalaprovince={}
+                    var style_st1_219313_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(127,229,149,1.0)"})
+                        })
+                        ];
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+                    var selectionStyle_st1_219313_waterextent_sofalaprovince = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ]
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+
+                    var textStyleCache_mozambique_osm2={}
+                    var clusterStyleCache_mozambique_osm2={}
+                    var style_mozambique_osm2 = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(149,83,84,1.0)", lineDash: null, width: 0})
+                        })
+                        ];
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+                    var selectionStyle_mozambique_osm2 = function(feature, resolution){
+                        
+                        var value = "";
+                        var style = [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0})
+                        })
+                        ]
+                        var allStyles = [];
+                        
+                        allStyles.push.apply(allStyles, style);
+                        return allStyles;
+                    };
+var baseLayers = [];var baseLayersGroup = new ol.layer.Group({showContent: true,'type':
+                    'base-group', 'title': 'Base maps', layers: baseLayers});
+var overlayLayers = [new ol.layer.Tile({
+	type: 'base-overlay',
+	title: 'Hydda roads and labels',
+	source: new ol.source.XYZ({
+		url: 'http://s.tile.openstreetmap.se/hydda/roads_and_labels/{z}/{x}/{y}.png'
+	}),
+    projection: 'EPSG:3857'
+})
+];var overlaysGroup = new ol.layer.Group({showContent: true, 'title': 'Overlays', layers: overlayLayers});
+var lyr_caltitude = new ol.layer.Image({
+                                opacity: 1.0,
+                                 
+                                title: "cAltitude",
+                                id: "cAltitude20190322152535120",
+                                timeInfo: null,
+                                source: new ol.source.ImageStatic({
+                                   url: "./data/caltitude.jpg",
+                                    projection: "EPSG:3857",
+                                    alwaysInRange: true,
+                                    imageSize: [527, 400],
+                                    imageExtent: [3612317.331749, -2425695.203171, 4101195.409261, -2031696.877579]
+                                })
+                            });
+var lyr_clandcover = new ol.layer.Image({
+                                opacity: 1.0,
+                                 
+                                title: "cLandCover",
+                                id: "cLandCover20190322152535190",
+                                timeInfo: null,
+                                source: new ol.source.ImageStatic({
+                                   url: "./data/clandcover.jpg",
+                                    projection: "EPSG:3857",
+                                    alwaysInRange: true,
+                                    imageSize: [527, 400],
+                                    imageExtent: [3612317.331749, -2425695.203171, 4101195.409261, -2031696.877579]
+                                })
+                            });
+var lyr_st1_21932_waterextent_sofalaprovince = new ol.layer.Vector({
+                    opacity: 1.0,
+                    source: new ol.source.Vector(),
+                     
+                    style: style_st1_21932_waterextent_sofalaprovince,
+                    selectedStyle: selectionStyle_st1_21932_waterextent_sofalaprovince,
+                    title: "ST1_20190320_WaterExtent_SofalaProvince",
+                    id: "ST1_20190320_WaterExtent_SofalaProvince20190322152525143",
+                    filters: [],
+                    timeInfo: null,
+                    isSelectable: true,
+                    popupInfo: ""
+                });
+st1_21932_waterextent_sofalaprovince_geojson_callback = function(geojson) {
+                              lyr_st1_21932_waterextent_sofalaprovince.getSource().addFeatures(new ol.format.GeoJSON().readFeatures(geojson));
+                        };
+var lyr_st1_219319_waterextent_sofalaprovince = new ol.layer.Vector({
+                    opacity: 1.0,
+                    source: new ol.source.Vector(),
+                     
+                    style: style_st1_219319_waterextent_sofalaprovince,
+                    selectedStyle: selectionStyle_st1_219319_waterextent_sofalaprovince,
+                    title: "ST1_20190319_WaterExtent_SofalaProvince",
+                    id: "ST1_20190319_WaterExtent_SofalaProvince20190322152525130",
+                    filters: [],
+                    timeInfo: null,
+                    isSelectable: true,
+                    popupInfo: ""
+                });
+st1_219319_waterextent_sofalaprovince_geojson_callback = function(geojson) {
+                              lyr_st1_219319_waterextent_sofalaprovince.getSource().addFeatures(new ol.format.GeoJSON().readFeatures(geojson));
+                        };
+var lyr_st1_219314_waterextent_sofalaprovince = new ol.layer.Vector({
+                    opacity: 1.0,
+                    source: new ol.source.Vector(),
+                     
+                    style: style_st1_219314_waterextent_sofalaprovince,
+                    selectedStyle: selectionStyle_st1_219314_waterextent_sofalaprovince,
+                    title: "ST1_20190314_WaterExtent_SofalaProvince",
+                    id: "ST1_20190314_WaterExtent_SofalaProvince20190322152525116",
+                    filters: [],
+                    timeInfo: null,
+                    isSelectable: true,
+                    popupInfo: ""
+                });
+st1_219314_waterextent_sofalaprovince_geojson_callback = function(geojson) {
+                              lyr_st1_219314_waterextent_sofalaprovince.getSource().addFeatures(new ol.format.GeoJSON().readFeatures(geojson));
+                        };
+var lyr_st1_219313_waterextent_sofalaprovince = new ol.layer.Vector({
+                    opacity: 1.0,
+                    source: new ol.source.Vector(),
+                     
+                    style: style_st1_219313_waterextent_sofalaprovince,
+                    selectedStyle: selectionStyle_st1_219313_waterextent_sofalaprovince,
+                    title: "ST1_20190313_WaterExtent_SofalaProvince",
+                    id: "ST1_20190313_WaterExtent_SofalaProvince20190322152524944",
+                    filters: [],
+                    timeInfo: null,
+                    isSelectable: true,
+                    popupInfo: ""
+                });
+st1_219313_waterextent_sofalaprovince_geojson_callback = function(geojson) {
+                              lyr_st1_219313_waterextent_sofalaprovince.getSource().addFeatures(new ol.format.GeoJSON().readFeatures(geojson));
+                        };
+var lyr_mozambique_osm2 = new ol.layer.Vector({
+                    opacity: 1.0,
+                    source: new ol.source.Vector(),
+                     
+                    style: style_mozambique_osm2,
+                    selectedStyle: selectionStyle_mozambique_osm2,
+                    title: "mozambique_osm2",
+                    id: "mozambique_osm220190322154208529",
+                    filters: [],
+                    timeInfo: null,
+                    isSelectable: true,
+                    popupInfo: ""
+                });
+mozambique_osm2_geojson_callback = function(geojson) {
+                              lyr_mozambique_osm2.getSource().addFeatures(new ol.format.GeoJSON().readFeatures(geojson));
+                        };
+
+lyr_caltitude.setVisible(true);
+lyr_clandcover.setVisible(true);
+lyr_st1_21932_waterextent_sofalaprovince.setVisible(true);
+lyr_st1_219319_waterextent_sofalaprovince.setVisible(true);
+lyr_st1_219314_waterextent_sofalaprovince.setVisible(true);
+lyr_st1_219313_waterextent_sofalaprovince.setVisible(true);
+lyr_mozambique_osm2.setVisible(true);
+var layersList = [lyr_caltitude,lyr_clandcover,lyr_st1_21932_waterextent_sofalaprovince,lyr_st1_219319_waterextent_sofalaprovince,lyr_st1_219314_waterextent_sofalaprovince,lyr_st1_219313_waterextent_sofalaprovince,lyr_mozambique_osm2];layersList.push(overlaysGroup);
+var view = new ol.View({ maxZoom: 32, minZoom: 1, projection: 'EPSG:3857'});
+var originalExtent = [3583440.996105, -2423411.568200, 4139496.026915, -2027975.961311];
+
+var map = new ol.Map({
+  layers: layersList,
+  view: view,
+  controls: []
+});
+
+
+
+var BasicApp = React.createClass({
+  componentDidMount() {
+    map.setTarget(ReactDOM.findDOMNode(this.refs.map));
+    view = map.getView();
+    view.fit(originalExtent, map.getSize());
+    
+  },
+  _toggle(el) {
+    if (el.style.display === 'block') {
+      el.style.display = 'none';
+    } else {
+      el.style.display = 'block';
+    }
+  },
+  _toggleTable() {
+    this._toggle(document.getElementById('table-panel'));
+    this.refs.table.getWrappedInstance().setDimensionsOnState();
+  },
+  _toggleWFST() {
+    this._toggle(document.getElementById('wfst'));
+  },
+  _toggleQuery() {
+    this._toggle(document.getElementById('query-panel'));
+  },
+  _toggleEdit() {
+    this._toggle(document.getElementById('edit-tool-panel'));
+  },
+  _toggleAboutPanel() {
+    this._toggle(document.getElementById('about-panel'));
+  },
+  _toggleChartPanel(evt) {
+    evt.preventDefault();
+    this._toggle(document.getElementById('chart-panel'));
+  },
+  _navigationFunc() {
+    ToolActions.activateTool(null, 'navigation');
+  },
+  render() {
+    var options = [{text: 'Table', icon: 'list-alt', onClick: this._toggleTable.bind(this)},
+{text: 'Query', icon: 'filter', onClick: this._toggleQuery.bind(this)},
+{jsx: React.createElement(Measure, {toggleGroup:'navigation', map:map})},
+{
+                              jsx: React.createElement(Select, {toggleGroup: 'navigation', map:map})
+                            }, {
+                              text: 'Navigation',
+                              icon: 'hand-paper-o',
+                              onClick: this._navigationFunc.bind(this)
+                            },
+{exclude: true, jsx: React.createElement("a", {className:"navbar-brand", href:"#"}, "Mozambique")}];
+    return React.createElement("article", null,
+      React.createElement(Toolbar, {options: options}
+      ),
+      React.createElement("div", {id: 'content'},
+        React.createElement("div", {id: 'map', ref: 'map'}
+          ,
+React.createElement("div", {id: 'query-panel', className:'query-panel'},
+                                          React.createElement(QueryBuilder, {map: map})
+                                        ),
+React.createElement("div", {id: 'popup', className: 'ol-popup'},
+                                    React.createElement(InfoPopup, {map: map, hover: false})
+                                  )
+        )
+        ,
+ React.createElement("div", {id: 'table-panel', className: 'attributes-table'},
+                                          React.createElement(FeatureTable, {offset: [20, 20], ref: 'table', resizeTo: 'table-panel', layer: lyr_st1_21932_waterextent_sofalaprovince, pointZoom:16, map: map})
+                                    ),
+React.createElement("div",{id: "layerlist"},
+                                    React.createElement(LayerList, {showOpacity:false, showDownload:false,
+                                        showGroupContent:true, showZoomTo:false, allowReordering:false,
+                                        allowFiltering:true, tipLabel:'layers', expandOnHover:true,
+                                        downloadFormat:'GeoJSON', map:map}))
+      )
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(IntlProvider, {locale: 'en'}, React.createElement(BasicApp)), document.getElementById('main'));
+
+
